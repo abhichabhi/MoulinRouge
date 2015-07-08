2593096ef62c33dd2eec3e793fd5fc926c74ad43
StratDecider <dude.abhi.chat@gmail.com>
2015-07-09 04:18:21 +0530

    core Classes for Email Service

--- TestEmail.py
+++ TestEmail.py
@@ -8,6 +8,6 @@ values['from'] = 'The Team'
 values['url'] = 'http://www.bazaarfunda.com'
 temp = EmailTemplate(template_name='TestTemplate.html', values=values)
 
-server = MailServer(server_name='smtp.gmail.com', username='bazaarfunda@gmail.com', password='krish1436', port=0,   require_starttls=True)
+server = MailServer(server_name='smtp.gmail.com', username='*********', password='*******', port=0,   require_starttls=True)
 msg = MailMessage(from_email='dude.abhi.chat@gmail.com', to_emails=['dude.abhi.chat@gmail.com'], subject='Welcome')
 send(mail_msg=msg, mail_server=server, template=temp)
